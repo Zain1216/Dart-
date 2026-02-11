c7cf182e781db6edc68d729b0c25791f3e6177b3
Zainul Abideen <121451360+Zain1216@users.noreply.github.com>
2026-02-11 20:14:55 +0500

    Add files via upload

---
+++ first.dart
@@ -0,0 +1,20 @@
+void main(){
+  print ("hello Umer");
+
+  const a = 12;
+  const b = 20;
+
+print(a + b);
+print(a / b);
+
+print(true);
+
+int l = 20;
+int c = 10;
+
+print ("the sum of $l & $b is ${l + c}");
+print ("the area of $l & $b is ${l * c}");
+
+
+
+}
---
+++ second.dart
@@ -0,0 +1,32 @@
+void main(){
+
+var salary = 12000;
+
+if (salary > 20000){
+print("you got promtion🙈😎😚✅🎂");
+} else {
+  print("you need improvement");
+}
+
+var marks = 100;
+
+if (marks >= 90 && marks < 100){
+  print ("A+ Grade");
+} else if (marks >= 50 && marks < 100){
+print("B Grade");
+} else if (marks >= 0 && marks < 50){
+  print("you are fail");
+} else {
+  print("invalid");
+}
+const b = null;
+
+String? Newuserinput = b ;
+String Namedisplay = Newuserinput ?? "Guest User";
+print(Namedisplay);
+
+
+
+
+
+} 
