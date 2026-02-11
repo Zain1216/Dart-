cf6d7b157fbeea240c1cb16aa1fae46bb589ba0f
Zainul Abideen <121451360+Zain1216@users.noreply.github.com>
2026-02-11 20:45:21 +0500

    Refactor main function and add outer loop

--- second.dart
+++ second.dart
@@ -25,7 +25,29 @@ String? Newuserinput = b ;
 String Namedisplay = Newuserinput ?? "Guest User";
 print(Namedisplay);
 
+myouterloop:
+for (int i = 1; i <= 3; i++){
+  innerloop:
+  for (int j = 1; j <= 3; j++){
+    print("$i $j");
 
+    if (i == 2 && j == 2){
+      break myouterloop;
+    }
+  }
+}
+
+print("fiyfd");
+
+myouterloop:
+for (int i = 1; i <= 3; i++){
+  innerloop:
+  for (int j = 1; j <= 3; j++){
+    print("$i $j");
+
+    
+  }
+}
 
 
 
